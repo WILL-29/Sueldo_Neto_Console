ed092fa59bf8930579b3e6d37ce77e50ecf5cc97
WILL-29 <77632837+WILL-29@users.noreply.github.com>
2021-10-25 14:08:49 -0400

    AFS + ARS + Primera escala ISR
    Se creó la clase y los métodos para calcular AFP, ARS y la primera escala del ISR

---
+++ Sueldo_Neto_Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sueldo_Neto_Console
+{
+    class Program : claNomina
+    {
+        static void Main(string[] args)
+        {
+            Double sb;
+            sb = 50000;
+            Console.WriteLine($"De Seguro de salud:   {Convert.ToString(claNomina.DescARS(sb))}");
+            Console.WriteLine($"De AFP se descontará: {Convert.ToString(claNomina.DescAFP(sb))}");
+            Console.WriteLine($"De ISR:               {Convert.ToString(claNomina.DescISR(sb))}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Sueldo_Neto_Console/claNomina.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sueldo_Neto_Console
+{
+    class claNomina
+    {
+        public static double DescARS(double sb)
+        {
+            //Esto en realidad se llama SFS, Seguro Familiar de Salud
+            double Total_Desc;
+            Total_Desc = sb - (sb * 0.9696);
+            return Total_Desc;
+        }
+        public static double DescAFP(double sb)
+        {
+            Double Total_Desc;
+            Total_Desc = sb - (sb * 0.9713);
+            return Total_Desc;
+        }
+        public static double DescISR(double sb)
+        {
+            double Total_Desc, Rest;
+            Rest = sb - DescAFP(sb) - DescARS(sb);
+            if (Rest > 34685.0 && Rest < 52027.42)
+            {
+                Total_Desc = ((Rest - 34685) * 15) / 100;
+                return Total_Desc;
+            }
+            else
+            {
+                return 9;
+            }
+
+        }
+    }
+}
